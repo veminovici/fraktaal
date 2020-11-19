6b45bdd5c0e3cf400b5026e557593f6e64cc7345
VLAD EMINOVICI <veminovici@hotmail.com>
2020-11-19 23:27:58 +0200

    Add expecto

--- build.fsx
+++ build.fsx
@@ -1,14 +1,21 @@
 #r "paket:
 nuget Fake.DotNet.Cli
+nuget Fake.Testing.Common
+nuget Fake.DotNet.Testing.Expecto
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target //"
+
 #load ".fake/build.fsx/intellisense.fsx"
+#load "./expecto.fsx"
+
+open Expecto
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+open System
 
 Target.initEnvironment ()
 
@@ -20,13 +27,19 @@ Target.create "Clean" (fun _ ->
 
 Target.create "Build" (fun _ ->
     !! "src/**/*.*proj"
+    ++ "tests/**/*.*proj"
     |> Seq.iter (DotNet.build id)
 )
+
+Target.create "Expecto" (fun _ ->
+    !! "tests/xpect/bin/Release/**/xpect.exe"
+    |> runExpecto id)
 
 Target.create "All" ignore
 
 "Clean"
   ==> "Build"
+  ==> "Expecto"
   ==> "All"
 
 Target.runOrDefault "All"
---
+++ tests/xpect/Main.fs
@@ -0,0 +1,6 @@
+﻿module xpect
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
